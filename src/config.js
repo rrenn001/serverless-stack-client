a47f9c8629c92253a00acc0bbf26bf0adfa67e7d
Richard Renner <richard.renner@tcorp.nsw.gov.au>
2020-01-28 12:33:02 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-uploads-au"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://quk9rz5ev9.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_oXcHqs3VU",
+    APP_CLIENT_ID: "728877bb9m1lvkk83ti3dm2pn6",
+    IDENTITY_POOL_ID: "ap-southeast-2:578124bb-2e17-42c8-a1cd-19cb0d5b3350"
+  }
+};
